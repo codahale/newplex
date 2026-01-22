f287e421d856a4072caa2eeeac21579f9118a018
Coda Hale <coda.hale@gmail.com>
2026-01-21 18:20:28 -0700

    Replace Simpira tests with fuzz tests.

--- internal/simpira/simpira_test.go
+++ internal/simpira/simpira_test.go
@@ -2,75 +2,116 @@ package simpira //nolint:testpackage // need access to generic impl
 
 import (
 	"bytes"
+	"crypto/sha3"
 	"testing"
 )
 
-func TestPermute2(t *testing.T) {
-	state1 := [32]byte{}
-	for i := range 32 {
-		state1[i] = byte(i)
-	}
-	state2 := state1
+func FuzzPermute2(f *testing.F) {
+	const width = 32
+	rng := sha3.NewSHAKE128()
+	_, _ = rng.Write([]byte("simpira-2-v2"))
 
-	Permute2(&state1)
-	permute2Generic(&state2)
-
-	if !bytes.Equal(state1[:], state2[:]) {
-		t.Errorf("Permute2 assembly results don't match generic implementation")
-		t.Errorf("Asm: %x", state1)
-		t.Errorf("Gen: %x", state2)
+	for range 10 {
+		state := make([]byte, width)
+		_, _ = rng.Read(state)
+		f.Add(state)
 	}
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != width {
+			t.Skip()
+		}
+
+		var state1, state2 [width]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
+		Permute2(&state1)
+		permute2Generic(&state2)
+
+		if got, want := state2[:], state1[:]; !bytes.Equal(got, want) {
+			t.Errorf("Permute2-ASM(%x) = %x, want = %x", data, got, want)
+		}
+	})
 }
 
-func TestPermute4(t *testing.T) {
-	state1 := [64]byte{}
-	for i := range 64 {
-		state1[i] = byte(i)
-	}
-	state2 := state1
+func FuzzPermute4(f *testing.F) {
+	const width = 64
+	rng := sha3.NewSHAKE128()
+	_, _ = rng.Write([]byte("simpira-4-v2"))
 
-	Permute4(&state1)
-	permute4Generic(&state2)
-
-	if !bytes.Equal(state1[:], state2[:]) {
-		t.Errorf("Permute4 assembly results don't match generic implementation")
-		t.Errorf("Asm: %x", state1)
-		t.Errorf("Gen: %x", state2)
+	for range 10 {
+		state := make([]byte, width)
+		_, _ = rng.Read(state)
+		f.Add(state)
 	}
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != width {
+			t.Skip()
+		}
+
+		var state1, state2 [width]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
+		Permute4(&state1)
+		permute4Generic(&state2)
+
+		if got, want := state2[:], state1[:]; !bytes.Equal(got, want) {
+			t.Errorf("Permute4-ASM(%x) = %x, want = %x", data, got, want)
+		}
+	})
 }
 
-func TestPermute6(t *testing.T) {
-	state1 := [96]byte{}
-	for i := range 96 {
-		state1[i] = byte(i)
-	}
-	state2 := state1
+func FuzzPermute6(f *testing.F) {
+	const width = 96
+	rng := sha3.NewSHAKE128()
+	_, _ = rng.Write([]byte("simpira-6-v2"))
 
-	Permute6(&state1)
-	permute6Generic(&state2)
-
-	if !bytes.Equal(state1[:], state2[:]) {
-		t.Errorf("Permute6 assembly results don't match generic implementation")
-		t.Errorf("Asm: %x", state1)
-		t.Errorf("Gen: %x", state2)
+	for range 10 {
+		state := make([]byte, width)
+		_, _ = rng.Read(state)
+		f.Add(state)
 	}
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != width {
+			t.Skip()
+		}
+
+		var state1, state2 [width]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
+		Permute6(&state1)
+		permute6Generic(&state2)
+
+		if got, want := state2[:], state1[:]; !bytes.Equal(got, want) {
+			t.Errorf("Permute6-ASM(%x) = %x, want = %x", data, got, want)
+		}
+	})
 }
 
-func TestPermute8(t *testing.T) {
-	state1 := [128]byte{}
-	for i := range 128 {
-		state1[i] = byte(i)
-	}
-	state2 := state1
+func FuzzPermute8(f *testing.F) {
+	const width = 128
+	rng := sha3.NewSHAKE128()
+	_, _ = rng.Write([]byte("simpira-8-v2"))
 
-	Permute8(&state1)
-	permute8Generic(&state2)
-
-	if !bytes.Equal(state1[:], state2[:]) {
-		t.Errorf("Permute8 assembly results don't match generic implementation")
-		t.Errorf("Asm: %x", state1)
-		t.Errorf("Gen: %x", state2)
+	for range 10 {
+		state := make([]byte, width)
+		_, _ = rng.Read(state)
+		f.Add(state)
 	}
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != width {
+			t.Skip()
+		}
+
+		var state1, state2 [width]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
+		Permute8(&state1)
+		permute8Generic(&state2)
+
+		if got, want := state2[:], state1[:]; !bytes.Equal(got, want) {
+			t.Errorf("Permute8-ASM(%x) = %x, want = %x", data, got, want)
+		}
+	})
 }
 
 func BenchmarkPermute2(b *testing.B) {
