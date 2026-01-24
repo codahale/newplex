936480a328cf37ec95b479e41ce953d2ad670b04
Coda Hale <coda.hale@gmail.com>
2026-01-24 16:07:52 -0700

    Replace aesni with a bitsliced impl.

--- internal/aesni/aesni.go
+++ internal/aesni/aesni.go
@@ -1,69 +1,177 @@
 package aesni
 
+func pack(s [16]byte) (q [8]uint16) {
+	for i := range 16 {
+		b := uint16(s[i])
+		m := uint16(1) << i
+		q[0] |= (b & 1) * m
+		q[1] |= ((b >> 1) & 1) * m
+		q[2] |= ((b >> 2) & 1) * m
+		q[3] |= ((b >> 3) & 1) * m
+		q[4] |= ((b >> 4) & 1) * m
+		q[5] |= ((b >> 5) & 1) * m
+		q[6] |= ((b >> 6) & 1) * m
+		q[7] |= ((b >> 7) & 1) * m
+	}
+	return q
+}
+
+func unpack(q [8]uint16) (s [16]byte) {
+	for i := range 16 {
+		m := uint16(1) << i
+		b := (q[0] & m) >> i
+		b |= ((q[1] & m) >> i) << 1
+		b |= ((q[2] & m) >> i) << 2
+		b |= ((q[3] & m) >> i) << 3
+		b |= ((q[4] & m) >> i) << 4
+		b |= ((q[5] & m) >> i) << 5
+		b |= ((q[6] & m) >> i) << 6
+		b |= ((q[7] & m) >> i) << 7
+		s[i] = byte(b)
+	}
+	return s
+}
+
+// AESENC performs one round of AES encryption.
 func AESENC(state, key [16]byte) [16]byte {
-	subBytes(&state)
-	shiftRows(&state)
-	mixColumns(&state)
+	q := pack(state)
+	q = sbox(q)
+	q = shiftRows(q)
+	q = mixColumns(q)
+	state = unpack(q)
 	for i := range 16 {
 		state[i] ^= key[i]
 	}
 	return state
 }
 
+// AESENCLAST performs the last round of AES encryption.
 func AESENCLAST(state, key [16]byte) [16]byte {
-	subBytes(&state)
-	shiftRows(&state)
+	q := pack(state)
+	q = sbox(q)
+	q = shiftRows(q)
+	state = unpack(q)
 	for i := range 16 {
 		state[i] ^= key[i]
 	}
 	return state
 }
 
-var sbox = [256]byte{ //nolint:gochecknoglobals // too big to initialize dynamically
-	0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-	0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-	0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-	0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
-	0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
-	0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
-	0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
-	0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
-	0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
-	0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
-	0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
-	0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
-	0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-	0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
-	0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-	0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
-}
-
-func subBytes(state *[16]byte) {
-	for i := range 16 {
-		state[i] = sbox[state[i]]
+func shiftRows(q [8]uint16) [8]uint16 {
+	// ShiftRows permutes bits in each q_i.
+	var r [8]uint16
+	rot := func(in uint16) uint16 {
+		return (in & 0x1111) |
+			((in & 0x2220) >> 4) | ((in & 0x0002) << 12) |
+			((in & 0x4400) >> 8) | ((in & 0x0044) << 8) |
+			((in & 0x0888) << 4) | ((in & 0x8000) >> 12)
 	}
-}
-
-func shiftRows(state *[16]byte) {
-	s := *state
-	state[1], state[5], state[9], state[13] = s[5], s[9], s[13], s[1]
-	state[2], state[6], state[10], state[14] = s[10], s[14], s[2], s[6]
-	state[3], state[7], state[11], state[15] = s[15], s[3], s[7], s[11]
-}
-
-func mul2(b byte) byte {
-	if b&0x80 != 0 {
-		return (b << 1) ^ 0x1b
+	for i := range 8 {
+		r[i] = rot(q[i])
 	}
-	return b << 1
+	return r
 }
 
-func mixColumns(state *[16]byte) {
-	for i := range 4 {
-		s0, s1, s2, s3 := state[i*4], state[i*4+1], state[i*4+2], state[i*4+3]
-		state[i*4] = mul2(s0) ^ (mul2(s1) ^ s1) ^ s2 ^ s3
-		state[i*4+1] = s0 ^ mul2(s1) ^ (mul2(s2) ^ s2) ^ s3
-		state[i*4+2] = s0 ^ s1 ^ mul2(s2) ^ (mul2(s3) ^ s3)
-		state[i*4+3] = (mul2(s0) ^ s0) ^ s1 ^ s2 ^ mul2(s3)
+func mixColumns(q [8]uint16) [8]uint16 {
+	// xtime: multiplication by 2 in GF(2^8).
+	t0 := q[7]
+	t1 := q[0] ^ q[7]
+	t2 := q[1]
+	t3 := q[2] ^ q[7]
+	t4 := q[3] ^ q[7]
+	t5 := q[4]
+	t6 := q[5]
+	t7 := q[6]
+
+	t := [8]uint16{t0, t1, t2, t3, t4, t5, t6, t7}
+
+	// Rotations for MixColumns (cyclic shift of nibbles)
+	rot1 := func(x uint16) uint16 { return (x>>1)&0x7777 | (x&0x1111)<<3 }
+	rot2 := func(x uint16) uint16 { return (x>>2)&0x3333 | (x&0x3333)<<2 }
+	rot3 := func(x uint16) uint16 { return (x>>3)&0x1111 | (x&0x7777)<<1 }
+
+	var r [8]uint16
+	for k := range 8 {
+		r[k] = t[k] ^ rot1(t[k]^q[k]) ^ rot2(q[k]) ^ rot3(q[k])
 	}
+
+	return r
+}
+
+func mul(a, b [8]uint16) [8]uint16 {
+	var p [15]uint16
+	for i := range 8 {
+		for j := range 8 {
+			p[i+j] ^= a[i] & b[j]
+		}
+	}
+	// Reduce modulo x^8 + x^4 + x^3 + x + 1
+	for i := 14; i >= 8; i-- {
+		v := p[i]
+		p[i-4] ^= v
+		p[i-5] ^= v
+		p[i-7] ^= v
+		p[i-8] ^= v
+	}
+	var res [8]uint16
+	for i := range 8 {
+		res[i] = p[i]
+	}
+	return res
+}
+
+func sq(a [8]uint16) [8]uint16 {
+	var p [15]uint16
+	for i := range 8 {
+		p[2*i] = a[i]
+	}
+	// Reduce
+	for i := 14; i >= 8; i-- {
+		v := p[i]
+		p[i-4] ^= v
+		p[i-5] ^= v
+		p[i-7] ^= v
+		p[i-8] ^= v
+	}
+	var res [8]uint16
+	for i := range 8 {
+		res[i] = p[i]
+	}
+	return res
+}
+
+func inv(a [8]uint16) [8]uint16 {
+	// x^254 using addition chain
+	x2 := sq(a)
+	x4 := sq(x2)
+	x8 := sq(x4)
+	x16 := sq(x8)
+	x32 := sq(x16)
+	x64 := sq(x32)
+	x128 := sq(x64)
+
+	res := x2
+	res = mul(res, x4)
+	res = mul(res, x8)
+	res = mul(res, x16)
+	res = mul(res, x32)
+	res = mul(res, x64)
+	res = mul(res, x128)
+	return res
+}
+
+func affine(a [8]uint16) [8]uint16 {
+	var s [8]uint16
+	for i := range 8 {
+		s[i] = a[i] ^ a[(i+4)%8] ^ a[(i+5)%8] ^ a[(i+6)%8] ^ a[(i+7)%8]
+	}
+	s[0] = ^s[0]
+	s[1] = ^s[1]
+	s[5] = ^s[5]
+	s[6] = ^s[6]
+	return s
+}
+
+func sbox(u [8]uint16) [8]uint16 {
+	return affine(inv(u))
 }
