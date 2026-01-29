086fd099f15fbce27f61a2b0b156d93c2f3956c8
Coda Hale <coda.hale@gmail.com>
2026-01-28 20:29:10 -0700

    More duplex test refactoring.

--- duplex_test.go
+++ duplex_test.go
@@ -8,42 +8,65 @@ import (
 	"github.com/codahale/newplex"
 )
 
-func TestDuplex_Permute(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
-
-	if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
-		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
-	}
-
-	d.Permute()
-
-	if got, want := newplex.State(&d), "^833a396d75b724a025afa7efda2c35beef0c7ab0f468874d1556fc1f92989fa76f36adc5a0f68f3d2da03d19a84ae4380528de5800187836dd604febeb3321a25fc4e1096e95dc6c17c806b81911c3d76624b8df38ff8ee2b6aa7727af63ce5e|7842524c4c22b359a254f465950454a26607efd25b116f83b4da2aafb89fd388"; got != want {
-		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
-	}
-}
-
 func TestDuplex_Absorb(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
+	t.Run("simple", func(t *testing.T) {
+		d := exampleDuplex()
 
-	if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
-		t.Errorf("state = %s,\nwant  = %s", got, want)
-	}
+		if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
+			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
+		}
+	})
 
-	d.Absorb(slices.Repeat[[]byte]([]byte{22}, 423))
+	t.Run("multi-block", func(t *testing.T) {
+		var d newplex.Duplex
+		d.Absorb(slices.Repeat([]byte{0xde, 0xad, 0xbe, 0xef}, 340))
 
-	if got, want := newplex.State(&d), "e9346d5b67a5a50fdc584d24e34848f8929669e865d7565ca80cde062c2f1a7e4fd8a493ed16457db8f7ad374a1bdc63b9^4d0dabfe77487ccabe1c98c052795f076cc090c97e36fbb84818bfe9457bedc6488b4331b8d971e4fc38a1184b4b7c|88a5aad01cff897fe47d56e845a8730eb5c4a7239fabc12c5d41caaa6f7a78e5"; got != want {
-		t.Errorf("state = %s,\nwant  = %s", got, want)
-	}
+		if got, want := newplex.State(&d), "9410c2dd059c2248b94cf4d727eb4094^f92fe19f82b841d41897bec9c86d2c6d8e49e60b3955f3015d83b9f9f30f61cd7410f43a50a7b1c2677e668decf3ed481bad40668c095998ea74fabeb8f9acbefd25b2c39ada05c405754611ad84a821|a1866172c3af451fa218257e3147e3678227ed4ffeef6ae2ede968086bf121f1"; got != want {
+			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
+		}
+	})
 }
 
 func TestDuplex_Squeeze(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
+	t.Run("simple", func(t *testing.T) {
+		var d newplex.Duplex
+		d.Permute()
+
+		if got, want := newplex.State(&d), "^5a7d4c12b2c4483055c5125c73c98edd8ae680baed946a6a42d52bc714f08c5f86d37c6b2e1840f17c8872add1068f5d17d120e2b00ffa0e5513874e92db2c29a4254192dd6eea69e00c38c7240606d8e92c475ee701b669138309d96f93ff2d|9313436f5ec7655c26d9674a98fe583974fc76ddc75185816cd3121104a87778"; got != want {
+			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
+		}
+
+		out := make([]byte, 10)
+		d.Squeeze(out)
+
+		if got, want := newplex.State(&d), "5a7d4c12b2c4483055c5^125c73c98edd8ae680baed946a6a42d52bc714f08c5f86d37c6b2e1840f17c8872add1068f5d17d120e2b00ffa0e5513874e92db2c29a4254192dd6eea69e00c38c7240606d8e92c475ee701b669138309d96f93ff2d|9313436f5ec7655c26d9674a98fe583974fc76ddc75185816cd3121104a87778"; got != want {
+			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
+		}
+
+		if got, want := hex.EncodeToString(out), "5a7d4c12b2c4483055c5"; got != want {
+			t.Errorf("squeeze = %s, want = %x", got, want)
+		}
+	})
+
+	t.Run("multi-block", func(t *testing.T) {
+		var d newplex.Duplex
+		d.Permute()
+
+		out := make([]byte, 2000)
+		d.Squeeze(out)
+
+		if got, want := newplex.State(&d), "f3ca4a1b698d5c0565c36cb863e6603582b8a5268878b636bef4badfa13138a88c46ba8f1a073d0798403782ac494e428d2c87dec1604138eee0ad1d5d7adc3943d4b507f662dfd90f9d9651244e5bc2^33d71d8fd721d40512415a1fb5af7f38|122060abae78e8a7d482283aa4c7a2e3041f649eb5cc92f615d12a46e51bd399"; got != want {
+			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
+		}
+
+		if got, want := hex.EncodeToString(out[:10]), "5a7d4c12b2c4483055c5"; got != want {
+			t.Errorf("squeeze = %s, want = %x", got, want)
+		}
+	})
+}
+
+func TestDuplex_Permute(t *testing.T) {
+	d := exampleDuplex()
 
 	if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -53,24 +76,11 @@ func TestDuplex_Squeeze(t *testing.T) {
 
 	if got, want := newplex.State(&d), "^833a396d75b724a025afa7efda2c35beef0c7ab0f468874d1556fc1f92989fa76f36adc5a0f68f3d2da03d19a84ae4380528de5800187836dd604febeb3321a25fc4e1096e95dc6c17c806b81911c3d76624b8df38ff8ee2b6aa7727af63ce5e|7842524c4c22b359a254f465950454a26607efd25b116f83b4da2aafb89fd388"; got != want {
 		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
-	}
-
-	out := make([]byte, 10)
-	d.Squeeze(out)
-
-	if got, want := newplex.State(&d), "833a396d75b724a025af^a7efda2c35beef0c7ab0f468874d1556fc1f92989fa76f36adc5a0f68f3d2da03d19a84ae4380528de5800187836dd604febeb3321a25fc4e1096e95dc6c17c806b81911c3d76624b8df38ff8ee2b6aa7727af63ce5e|7842524c4c22b359a254f465950454a26607efd25b116f83b4da2aafb89fd388"; got != want {
-		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
-	}
-
-	if got, want := hex.EncodeToString(out), "833a396d75b724a025af"; got != want {
-		t.Errorf("squeeze = %s, want = %x", got, want)
 	}
 }
 
 func TestDuplex_Ratchet(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
+	d := exampleDuplex()
 
 	if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 		t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -85,9 +95,7 @@ func TestDuplex_Ratchet(t *testing.T) {
 
 func TestDuplex_Encrypt(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
-		var d newplex.Duplex
-		d.Absorb([]byte{1, 2, 3, 4, 5})
-		d.Absorb([]byte{6, 7, 8, 9, 10})
+		d := exampleDuplex()
 
 		if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -109,9 +117,7 @@ func TestDuplex_Encrypt(t *testing.T) {
 	})
 
 	t.Run("in place", func(t *testing.T) {
-		var d newplex.Duplex
-		d.Absorb([]byte{1, 2, 3, 4, 5})
-		d.Absorb([]byte{6, 7, 8, 9, 10})
+		d := exampleDuplex()
 
 		if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -132,12 +138,13 @@ func TestDuplex_Encrypt(t *testing.T) {
 	})
 
 	t.Run("multi-block", func(t *testing.T) {
-		var d newplex.Duplex
+		d := exampleDuplex()
+
 		plaintext := make([]byte, 11*77*43)
 		ciphertext := make([]byte, len(plaintext))
 		d.Encrypt(ciphertext, plaintext)
 
-		if got, want := newplex.State(&d), "ec7429017e7a04ad165f81b1396326c99fb662198601602641e040cbe19d79e915308d019e^350e7bcd2c58fd137e5c9659ef0affd3ff2f4cbe1f4f98ba7a1e86af98a188db470a90dddeeda556cf86a3e33f288a346946dc7c33a8e1ffc7606d|ee9b15b98b1867d08b1024f107ce0d516bd03b4dc09c1e882f3b9136c584580a"; got != want {
+		if got, want := newplex.State(&d), "dce165b92db9242f0f61a503eee4377e2c39d212d2f5e44ffe72630e71fb56c60442579ba16fcdf1bc7b3fbc7fb6a1^3ef961ead2db50d98df2102b091982ce86e8af62d26f10df60f5c0bee347b19dee8dde016145786f3fb773501b810d116b|20876f28e9f617c047b2d698c080c5ca437b5d5a763bc97958030a1577b5f0b8"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
 		}
 	})
@@ -145,9 +152,7 @@ func TestDuplex_Encrypt(t *testing.T) {
 
 func TestDuplex_Decrypt(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
-		var d newplex.Duplex
-		d.Absorb([]byte{1, 2, 3, 4, 5})
-		d.Absorb([]byte{6, 7, 8, 9, 10})
+		d := exampleDuplex()
 
 		if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -169,9 +174,7 @@ func TestDuplex_Decrypt(t *testing.T) {
 	})
 
 	t.Run("in place", func(t *testing.T) {
-		var d newplex.Duplex
-		d.Absorb([]byte{1, 2, 3, 4, 5})
-		d.Absorb([]byte{6, 7, 8, 9, 10})
+		d := exampleDuplex()
 
 		if got, want := newplex.State(&d), "0102030405060708090a^0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|0000000000000000000000000000000000000000000000000000000000000000"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
@@ -192,21 +195,20 @@ func TestDuplex_Decrypt(t *testing.T) {
 	})
 
 	t.Run("multi-block", func(t *testing.T) {
-		var d newplex.Duplex
+		d := exampleDuplex()
+
 		ciphertext := make([]byte, 11*77*43)
 		plaintext := make([]byte, len(ciphertext))
 		d.Decrypt(plaintext, ciphertext)
 
-		if got, want := newplex.State(&d), "00000000000000000000000000000000000000000000000000000000000000000000000000^255a6cfcb14b0bdf8303379a789228e0e03cade68372657ba5659791cbb897ac9f64a0ca9351542d233edf4794d870b425e3ea44ac984ca1a25b77|c4ea4cb7762766db4e85dd39a2ffa17d2a19da1468473f92b55da2c8c0d5d805"; got != want {
+		if got, want := newplex.State(&d), "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000^fb2ca6cfa48194c3a56d68a96837af071e313a6dfa1e7d72e26fb4c4c1d57f162460f5907b0d9da53806135aa9fb8a216e|fd16957704e45cdcb0d8f9bd09cfb08e1bfa1a924ae3747dbd8304bc0e9b2a6d"; got != want {
 			t.Errorf("state = \n%s\nwant  = \n%s", got, want)
 		}
 	})
 }
 
 func TestDuplex_AppendBinary(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
+	d := exampleDuplex()
 
 	b, err := d.AppendBinary([]byte{22, 23})
 	if err != nil {
@@ -219,9 +221,7 @@ func TestDuplex_AppendBinary(t *testing.T) {
 }
 
 func TestDuplex_MarshalBinary(t *testing.T) {
-	var d newplex.Duplex
-	d.Absorb([]byte{1, 2, 3, 4, 5})
-	d.Absorb([]byte{6, 7, 8, 9, 10})
+	d := exampleDuplex()
 
 	b, err := d.MarshalBinary()
 	if err != nil {
@@ -262,6 +262,13 @@ func TestDuplex_UnmarshalBinary(t *testing.T) {
 			t.Errorf("error expected but none returned")
 		}
 	})
+}
+
+func exampleDuplex() newplex.Duplex {
+	var d newplex.Duplex
+	d.Absorb([]byte{1, 2, 3, 4, 5})
+	d.Absorb([]byte{6, 7, 8, 9, 10})
+	return d
 }
 
 func BenchmarkDuplex_Absorb(b *testing.B) {
