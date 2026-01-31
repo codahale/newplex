5e47b30c95f979c77edb346a63fe84757524a21d
Coda Hale <coda.hale@gmail.com>
2026-01-30 21:05:06 -0700

    Add aead package implementing cipher.AEAD
    This introduces the `aead` package, which provides a `crypto/cipher.AEAD` implementation backed by the Newplex protocol. It supports domain separation and requires a minimum nonce size of 16 bytes.
    
    Includes unit tests, benchmarks, and an example.

---
+++ aead/aead.go
@@ -0,0 +1,59 @@
+// Package aead provides an implementation of Authenticated Encryption with Associated Data (AEAD) using the Newplex
+// protocol.
+package aead
+
+import (
+	"crypto/cipher"
+
+	"github.com/codahale/newplex"
+)
+
+// New returns a new cipher.AEAD instance which uses the given domain string and key.
+func New(domain string, key []byte, nonceSize int) cipher.AEAD {
+	if nonceSize < 16 {
+		panic("newplex/aead: nonce size must be at least 16 bytes")
+	}
+	p := newplex.NewProtocol(domain)
+	p.Mix("key", key)
+	return &aead{
+		p:         p,
+		nonceSize: nonceSize,
+	}
+}
+
+type aead struct {
+	p         newplex.Protocol
+	nonceSize int
+}
+
+func (a *aead) NonceSize() int {
+	return a.nonceSize
+}
+
+func (a *aead) Overhead() int {
+	return newplex.TagSize
+}
+
+func (a *aead) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
+	if len(nonce) != a.NonceSize() {
+		panic("newplex/aead: invalid nonce size")
+	}
+
+	p := a.p.Clone()
+	p.Mix("nonce", nonce)
+	p.Mix("ad", additionalData)
+	return p.Seal("message", dst, plaintext)
+}
+
+func (a *aead) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
+	if len(nonce) != a.NonceSize() {
+		panic("newplex/aead: invalid nonce size")
+	}
+
+	p := a.p.Clone()
+	p.Mix("nonce", nonce)
+	p.Mix("ad", additionalData)
+	return p.Open("message", dst, ciphertext)
+}
+
+var _ cipher.AEAD = (*aead)(nil)
---
+++ aead/example_test.go
@@ -0,0 +1,32 @@
+package aead_test
+
+import (
+	"fmt"
+
+	"github.com/codahale/newplex/aead"
+)
+
+func Example() {
+	key := []byte("a very secret key, 32 bytes long")
+	nonce := []byte("a 16-byte nonce!")
+	ad := []byte("some additional data")
+	plaintext := []byte("hello world")
+
+	// Create a new AEAD instance with a 16-byte nonce.
+	c := aead.New("com.example.aead", key, 16)
+
+	// Seal the plaintext.
+	ciphertext := c.Seal(nil, nonce, plaintext, ad)
+	fmt.Printf("ciphertext = %x\n", ciphertext)
+
+	// Open the ciphertext.
+	decrypted, err := c.Open(nil, nonce, ciphertext, ad)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("plaintext  = %s\n", decrypted)
+
+	// Output:
+	// ciphertext = ad80e6a4150f9226888cf6a536c8b109334b9f75a1960cc9725138
+	// plaintext  = hello world
+}
