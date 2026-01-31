275a25087744eea28e96b31968b3bb5e473a0a4d
Coda Hale <coda.hale@gmail.com>
2026-01-31 07:36:17 -0700

    Add keyed digest example.

--- digest/example_test.go
+++ digest/example_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/codahale/newplex/digest"
 )
 
-func Example() {
+func Example_unkeyed() {
 	h := digest.New("com.example.digest")
 	_, _ = io.WriteString(h, "hello")
 	_, _ = io.WriteString(h, " world")
@@ -17,4 +17,16 @@ func Example() {
 
 	// Output:
 	// 6ba59cc7a456b062ca13f189ae7271bdce34506067f70d480f8d9c66bfcba8ce
+}
+
+func Example_keyed() {
+	h := digest.NewKeyed("com.example.mac", []byte("a secret key"))
+	_, _ = io.WriteString(h, "hello")
+	_, _ = io.WriteString(h, " world")
+
+	sum := h.Sum(nil)
+	fmt.Printf("%x\n", sum)
+
+	// Output:
+	// 03927b1b8a4ab6c162aa8a2e2df003ad
 }
