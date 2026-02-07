3e22d7d7b7d8477d0b8dcf5447f3897dcb005353
Coda Hale <coda.hale@gmail.com>
2026-02-06 18:26:16 -0700

    Add balloon package for memory-hard hashing
    Implement the Balloon hashing algorithm (https://eprint.iacr.org/2016/027.pdf) in a new `balloon` package. This provides a memory-hard hash function suitable for low-entropy secrets like passwords, built on top of `newplex.Protocol`.
    
    The implementation supports configurable space cost, time cost, and parallelism.
    
    Includes:
    *   `balloon.Hash`: The main hashing function.
    *   `ExampleHash`: A usage example in `balloon_test.go`.
    *   Updated `README.md` to list the new package.

---
+++ balloon/balloon_test.go
@@ -0,0 +1,22 @@
+package balloon_test
+
+import (
+	"crypto/sha3"
+	"fmt"
+
+	"github.com/codahale/newplex/balloon"
+)
+
+func ExampleHash() {
+	drbg := sha3.NewSHAKE128()
+	_, _ = drbg.Write([]byte("newplex balloon"))
+
+	password := []byte("secret stuff")
+	salt := make([]byte, 16)
+	_, _ = drbg.Read(salt)
+
+	hash := balloon.Hash("example", password, salt, 10*1024, 50, 4)
+	fmt.Printf("%x\n", hash)
+	// Output:
+	// f96ccea2ba507694406d99915f9ab72b5090b6e5b32b5c358eb9cfd8686587e3
+}
