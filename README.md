f3777221c27c445ac481c3721c56dc076c4cb7a3
Coda Hale <coda.hale@gmail.com>
2026-02-09 14:33:05 -0700

    Add OPRF implementation with example and documentation updates.
    Introduce an RFC 9497-style Oblivious Pseudorandom Function (OPRF) scheme in the `oprf` package, utilizing Newplex and Ristretto255. Include client-server blinding, evaluation, and finalization logic. Add tests and an example for client-server PRF derivation. Update `README.md` to reference the new package.

--- README.md
+++ README.md
@@ -85,6 +85,7 @@ Newplex includes many cryptographic schemes implemented as sub-packages:
 * [`newplex/digest`](digest): Implements `hash.Hash` (both keyed and unkeyed).
 * [`newplex/handshake`](handshake): Implements a mutually authenticated handshake.
 * [`newplex/hpke`](hpke): Implements a hybrid public-key encryption scheme.
+* [`newplex/oprf`](oprf): Implements an Opaque Pseudorandom Function scheme (OPF).
 * [`newplex/pake`](pake): Implements a CPace-style password-authenticated key exchange (PAKE).
 * [`newplex/sig`](sig): Implements EdDSA-style Schnorr digital signatures.
 * [`newplex/signcrypt`](signcrypt): Implements integrated public-key encryption and signing.
---
+++ oprf/oprf_test.go
@@ -0,0 +1,50 @@
+package oprf_test
+
+import (
+	"crypto/sha3"
+	"fmt"
+
+	"github.com/codahale/newplex/oprf"
+	"github.com/gtank/ristretto255"
+)
+
+func Example() {
+	drbg := sha3.NewSHAKE128()
+	_, _ = drbg.Write([]byte("newplex oprf"))
+	var r [64]byte
+	_, _ = drbg.Read(r[:])
+
+	// The server has a private key.
+	d, _ := ristretto255.NewScalar().SetUniformBytes(r[:])
+
+	// The client has a secret input and blinds it.
+	input := []byte("this is a sensitive input")
+	blind, blindedElement, err := oprf.Blind("example", input)
+	if err != nil {
+		panic(err)
+	}
+
+	// The server evaluates the blinded input.
+	evaluatedElement, err := oprf.BlindEvaluate(d, blindedElement)
+	if err != nil {
+		panic(err)
+	}
+
+	// The client finalizes it and derives PRF output.
+	clientPRF, err := oprf.Finalize("example", input, blind, evaluatedElement, 16)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("client PRF = %x\n", clientPRF)
+
+	// If the server gets the input, it can derive the same PRF output.
+	serverPRF, err := oprf.Evaluate("example", d, input, 16)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("server PRF = %x\n", serverPRF)
+
+	// Output:
+	// client PRF = ed927cc3f419a3875bb71fcae98d9a30
+	// server PRF = ed927cc3f419a3875bb71fcae98d9a30
+}
